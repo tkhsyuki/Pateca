542bea2eb15f183a94b3f7dd6819a5415e06a1ee
株式会社ツクモ <tsukumo296@gmail.com>
2020-10-25 16:00:41 +0900

    ver0.0

---
+++ PATECA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PATECA
+//
+//  Created by 高橋ゆうき on 2020/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
